5463dbfdb91322b281366200b77f5a72717993b0
Mikko Majander <mikko.majander@iki.fi>
2013-12-15 20:01:48 +0200

    Added pgp-encryption and removal of plain dump after encryption

--- backup_script_for_diaspora_db/diaspora_backup_db.sh
+++ backup_script_for_diaspora_db/diaspora_backup_db.sh
@@ -5,6 +5,8 @@ pidName='diaspora.pid'
 backupPath='/backup/diaspora/db/'
 backupName='diaspora_production_'
 
+encryptId='<basically_email_address_of_key_owner_here>'
+
 if test -s $pidName
 then
     # Pid found, exit
@@ -12,9 +14,19 @@ then
 else
     # Create pid
     echo $$ > diaspora.pid
+    
+    # Create name
+    backupName=$backupPath$backupName$(date +%Y-%m-%d_%H_%M_%S).sql
 
     # Take mysql dump
-    mysqldump -u root -p<password_here> diaspora_production > $backupPath$backupName$(date +%Y-%m-%d_%H_%M_%S).sql
+    mysqldump -u root -p<password_here> diaspora_production > $backupName
+    
+    # Encrypt the dump file
+    gpg -r $encryptId --out $backupName.secret --encrypt $backupName
+    
+    # Remove plain dump
+    rm $backupName
+    
 fi
 
 # Delete pid
